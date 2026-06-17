b33a278670796de910d74fa6d3b82319b7b3b7f2
Gowra Saketh <gowrasaketh14@gmail.com>
2026-06-17 14:36:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener("scroll", () => {
+    const navbar = document.querySelector(".top-nav");
+
+    if(window.scrollY > 50){
+        navbar.style.background = "rgba(0,0,0,0.9)";
+    }else{
+        navbar.style.background = "rgba(0,0,0,0.65)";
+    }
+});
